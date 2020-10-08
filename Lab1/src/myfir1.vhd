76d62cf8a814bad2cb1255af3bc78095521aaa66
= <=>
2020-10-08 17:44:04 +0200

    commit

--- Lab1/src/myfir1.vhd
+++ Lab1/src/myfir1.vhd
@@ -2,24 +2,24 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
-entity my_fir is
+entity myfir1 is
   port (
         CLK : in std_logic;
 	    RST_n : in std_logic;
-	    DIN : in signed(10 downto 0);
+	    DIN : in signed(15 downto 0);
         VIN : in std_logic;
-        H0      : in std_logic_vector(10 downto 0);
-        H1      : in std_logic_vector(10 downto 0);
-        H2      : in std_logic_vector(10 downto 0);
-        H3      : in std_logic_vector(10 downto 0);
+        H0      : in std_logic_vector(15 downto 0);
+        H1      : in std_logic_vector(15 downto 0);
+        H2      : in std_logic_vector(15 downto 0);
+        H3      : in std_logic_vector(15 downto 0);
         DOUT : out std_logic;
-        VOUT : out signed(10 downto 0)
+        VOUT : out signed(15 downto 0)
   ) ;
-end my_fir ; 
+end myfir1 ; 
 
-architecture beh of my_fir is
+architecture beh of myfir1 is
 
 begin
-    DOUT <= '1010101010';
-    VOUT <= '1';
+    DOUT <= '1';
+    VOUT <= "1010101010101010";
 end architecture ;
