ae4ebbddbea20a2dab533a4aa90af7400e5d24ef
Luigi <luigi.giuffrida.98@gmail.com>
2020-10-09 17:54:47 +0200

    up

--- Lab1/syn/syn.sh
+++ Lab1/syn/syn.sh
@@ -1,6 +1,16 @@
-#for f in $(ls ../src) ; do 
-#    arg+="$f "
-#done
-#echo $arg
+if [ -d work ]    # ← see 'man test' for available unary and binary operators.
+then
+    echo "Ho trovato la cartella work, per rendere deterministici i risultati procedo con la rimozione e la creazione della cartella"
+    rm -f work
+    mkdir work
+else
+    echo "La cartella work non è presente, la creo"
+    mkdir work
+fi
+
 source /software/scripts/init_synopsys_64.18
 design_vision -f synth.tcl
+dir="risultati_$(date +%F)"_"$(date +%T)"
+mkdir $dir
+mv ../synthReport dir
+mv ../netlist dir
