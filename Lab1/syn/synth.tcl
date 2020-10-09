36084135b3524722dc69d0f610e574347bb5aae8
Luigi <luigi.giuffrida.98@gmail.com>
2020-10-09 17:26:59 +0200

    up

--- Lab1/syn/synth.tcl
+++ Lab1/syn/synth.tcl
@@ -1,2 +1,32 @@
 set power preserve rtl hier names true
 analyze -f vhdl -lib WORK -autoread {../src}
+elaborate myfir -arch beh -lib WORK
+uniquify
+link
+create clock -name MY CLK -period 10.0 CLK
+set dont touch network MY CLK
+set clock uncertainty 0.07 [get clocks MY CLK]
+set input delay 0.5 -max -clock MY CLK [remove from collection [all inputs] CLK]
+set output delay 0.5 -max -clock MY CLK [all outputs]
+set OLOAD [load of NangateOpenCellLibrary/BUF X4/A]
+set load $OLOAD [all outputs]
+compile -exact_map
+#compile -exact_map -gate_clock
+
+#REPORTS
+
+report_timing > ../synthReport/timing.txt
+report_area  > ../synthReport/area.txt
+report_power > ../synthReport/power.txt
+report_power -net> ../synthReport/power-net.txt
+report_power -cell> ../synthReport/power-cell.txt
+report_power -hierarchy> ../synthReport/power-hierarchy.txt
+report_power -verbose> ../synthReport/power-verbose.txt
+
+#SDF
+
+ungroup -all -flatten
+change names -hierarchy -rules verilog
+write_sdf ../netlist/myfir.sdf
+write -f verilog -hierarchy -output ../netlist/myfir.v
+write_sdc ../netlist/myfir.sdc
