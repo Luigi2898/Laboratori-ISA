ec8273d69c713ffb1160220182af389bbfc81dfc
Luigi <luigi.giuffrida.98@gmail.com>
2020-10-10 12:26:31 +0200

    up

--- Lab1/syn/syn.sh
+++ Lab1/syn/syn.sh
@@ -18,7 +18,6 @@ fi
 
 source /software/scripts/init_synopsys_64.18
 design_vision -f synth.tcl
-dir="../OldRes/res_$(date +%F)"_"$(date +%T)"
 mkdir $dir
-cp synthReport $dir
-cp netlist $dir
+cp $dir synthReport
+cp $dir netlist
