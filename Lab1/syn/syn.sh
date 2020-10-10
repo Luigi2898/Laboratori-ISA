affd6c3ebffbc46d7e79a6add45cd141771f5b8c
Luigi <luigi.giuffrida.98@gmail.com>
2020-10-10 12:05:12 +0200

    update syn.sh

--- Lab1/syn/syn.sh
+++ Lab1/syn/syn.sh
@@ -1,7 +1,7 @@
 if [ -d work ]    # ← see 'man test' for available unary and binary operators.
 then
     echo "Ho trovato la cartella work, per rendere deterministici i risultati procedo con la rimozione e la creazione della cartella"
-    rm -f work
+    rm -rf work
     mkdir work
 else
     echo "La cartella work non è presente, la creo"
