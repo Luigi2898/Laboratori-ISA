497a13985d6f5e8f489eadfa22d6833a2991dc65
Luigi <luigi.giuffrida.98@gmail.com>
2020-10-10 12:48:10 +0200

    up

--- Lab1/syn/syn.sh
+++ Lab1/syn/syn.sh
@@ -1,6 +1,8 @@
 if [ -d work ]    # ← see 'man test' for available unary and binary operators.
 then
     echo "Ho trovato la cartella work, per rendere deterministici i risultati procedo con la rimozione e la creazione della cartella"
+    rm *.log
+    rm *.svf
     rm -rf work
     mkdir work
 else
