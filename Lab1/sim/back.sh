334d59ff1d96fd0f7107ed9d93d67bdd4c8119a7
Luigi <luigi.giuffrida.98@gmail.com>
2020-10-13 10:01:27 +0200

    Up

--- Lab1/sim/back.sh
+++ Lab1/sim/back.sh
@@ -15,7 +15,7 @@ cd ../syn
 source /software/scripts/init_synopsys_64.18
 vcd2saif -input ../vcd/myfir_syn.vcd -output ../saif/myfir_syn.saif
 design_vision -f backSyn.tcl
-dir="../OldRes/res_$(date +%F)"_"$(date +%T)"
+dir="../OldRes/res_$(date +%F)"_"$(date +%T)_synAndBack"
 mkdir $dir
 mv -r synthReport $dir 
 mv -r netlist $dir
