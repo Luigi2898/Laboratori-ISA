57a7997930cac7dc4b9786d7d24632d500782263
Luigi <luigi.giuffrida.98@gmail.com>
2020-10-20 17:18:08 +0200

    Up

--- Lab1/sim/back.sh
+++ Lab1/sim/back.sh
@@ -1,4 +1,5 @@
-if [ -d "work" ]; then
+backanno()
+{if [ -d "work" ]; then
 	rm -r work
 	echo "La cartella sim/work era presente ed è stata rimossa"
 fi
@@ -10,13 +11,17 @@ vcom -93 -work ./work ../src/data_sink.vhd
 vlog -work ./work ../syn/netlist/myfir.v
 vlog -work ./work ../tb/tb_fir.v
 vsim -do backVsim.tcl
+vsim -do backVsimNC.tcl
 cd ../syn
 source /software/scripts/init_synopsys_64.18
 vcd2saif -input ../vcd/myfir_syn.vcd -output ../saif/myfir_syn.saif
 dc_shell-xg-t -f backSyn.tcl
-dir="../OldRes/res_$(date +%F)"_"$(date +%T)_synAndBack"
+vcd2saif -input ../vcd/myfir_synNC.vcd -output ../saif/myfir_synNC.saif
+dc_shell-xg-t -f backSynNC.tcl $1
+dir="../OldRes/res_$(date +%F)"_"$(date +%H)"-"$(date +%M)"-"$(date +%S)_synAndBack"
 mkdir $dir
 mv synthReport $dir 
 mv netlist $dir
 mv logs $dir
 mv synthReportAfterBack $dir
+}
