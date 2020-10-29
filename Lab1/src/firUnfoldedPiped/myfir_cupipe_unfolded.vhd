06058146e935cecffc47044b33e0bd7e51ad17cd
Luigi <luigi.giuffrida.98@gmail.com>
2020-10-29 12:04:35 +0100

    Up

--- Lab1/src/firUnfoldedPiped/myfir_cupipe_unfolded.vhd
+++ Lab1/src/firUnfoldedPiped/myfir_cupipe_unfolded.vhd
@@ -1,73 +1,62 @@
-
 library IEEE;
-  use IEEE.STD_LOGIC_1164.all;
-  use IEEE.NUMERIC_STD.all;
+use IEEE.STD_LOGIC_1164.all;
+use IEEE.NUMERIC_STD.all;
 
-entity MYFIR_CUPIPE_UNFOLDED is
+entity myfir_cupipe_unfolded is
   port (
-    CLK        : in    std_logic;
-    RST_N      : in    std_logic;
-    START_PIPE : in    std_logic;
-    LOAD_RES   : out   std_logic;
-    START_OUT  : out   std_logic
-  );
-end entity MYFIR_CUPIPE_UNFOLDED;
+    clk : in std_logic;
+    rst_n : in std_logic;
+    start_pipe : in std_logic;
+    load_res : out std_logic;
+    start_out : out std_logic
+  ) ;
+end entity myfir_cupipe_unfolded;
 
-architecture BEH of MYFIR_CUPIPE_UNFOLDED is
+architecture beh of myfir_cupipe_unfolded is
 
-  type state_type is (rst, idle, start_outs);
-
-  signal state : state_type;
-  signal vdd   : std_logic;
+    type state_type is (rst,idle,start_outs);
+    signal state : state_type;
+    signal vdd : std_logic;
 
 begin
 
-  STATE_TRANSITION_PROCESS : process(clk, rst_n) is
-  begin
+    state_transition_process : process(clk,rst_n)
+    begin
+      if (rst_n = '0') then
+        state <= rst;
+      elsif (clk'event and clk = '1') then
+        case state is
+            when rst =>       state <= idle;
 
-    if (rst_n = '0') then
-      state <= rst;
-    elsif (clk'event and clk = '1') then
+            when idle =>        if (start_pipe = '1') then
+                                    state <= start_outs;
+                                else
+                                    state <= idle;
+                                end if;
 
+            when start_outs =>   state <= idle; 
+                            
+        end case;
+      end if;
+    end process;
+
+    output_evaluation_process: process(state)
+    begin
+
+    load_res <= '0';
+    start_out <= '0';
+  
       case state is
 
-        when rst =>
-          state <= idle;
+        when rst =>       
 
-        when idle =>
-          if (start_pipe = '1') then
-            state <= start_outs;
-          else
-            state <= idle;
-          end if;
+        when idle =>        
 
-        when start_outs =>
-          state <= idle;
+        when start_outs =>  load_res <= '1';
+                            start_out <= '1';
 
       end case;
 
-    end if;
+    end process output_evaluation_process;
 
-  end process STATE_TRANSITION_PROCESS;
-
-  OUTPUT_EVALUATION_PROCESS : process(state) is
-  begin
-
-    load_res  <= '0';
-    start_out <= '0';
-
-    case state is
-
-      when rst =>
-
-      when idle =>
-
-      when start_outs =>
-        load_res  <= '1';
-        start_out <= '1';
-
-    end case;
-
-  end process OUTPUT_EVALUATION_PROCESS;
-
-end architecture BEH;
+end architecture beh  ;
