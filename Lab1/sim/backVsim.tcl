ad105229668fe4fb3717858db8a944f1b728d7e0
Luigi <luigi.giuffrida.98@gmail.com>
2020-10-30 01:10:05 +0100

    I don't really know if it works, it's late to simulate, I'm going to bed, maybe can work, maybe not, if it does I'm happy

--- Lab1/sim/backVsim.tcl
+++ Lab1/sim/backVsim.tcl
@@ -1,5 +1,5 @@
 vsim -L /software/dk/nangate45/verilog/msim6.2g -sdftyp /tb_fir/UUT=../syn/netlist/myfir.sdf work.tb_fir
-vcd file ../vcd/myfir_syn.vcd
+vcd file ../vcd/fir/myfir_syn.vcd
 vcd add /tb_fir/UUT/*
 run 2 us
 quit -f
--- Lab1/syn/backSynNC.tcl
+++ Lab1/syn/backSynNC.tcl
@@ -1,13 +1,15 @@
-set fp [open "newPer.outGG" r]
+source ./definition.tcl
+
+set fp [open "newPer$normaltag.outGG" r]
 set newPer [read $fp]
 close $fp
-read_verilog -netlist ./netlist/myfirNC.v
+read_verilog -netlist ./netlist/firNC/myfir.v
 read_saif -input ../saif/myfir_synNC.saif -instance tb_fir/UUT -unit ns -scale 1
 create_clock -name MY_CLK -period $newPer CLK
-report_power > ./synthReportAfterBack/powerNC.txt
-report_power -net > ./synthReportAfterBack/power-netNC.txt
-report_power -cell > ./synthReportAfterBack/power-cellNC.txt
-report_power -hierarchy > ./synthReportAfterBack/power-hierarchyNC.txt
-report_power -verbose > ./synthReportAfterBack/power-verboseNC.txt
+report_power > ./synthReportAfterBack/firNC/power.txt
+report_power -net > ./synthReportAfterBack/firNC/power-net.txt
+report_power -cell > ./synthReportAfterBack/firNC/power-cell.txt
+report_power -hierarchy > ./synthReportAfterBack/firNC/power-hierarchy.txt
+report_power -verbose > ./synthReportAfterBack/firNC/power-verbose.txt
 exit
 
