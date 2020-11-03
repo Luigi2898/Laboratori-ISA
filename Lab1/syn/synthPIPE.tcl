8056d61a518efe3e47994b2ac54eaa54b8b93d1c
Luigi2898 <32927727+Luigi2898@users.noreply.github.com>
2020-11-03 11:51:16 +0100

    Update synthPIPE.tcl

--- Lab1/syn/synthPIPE.tcl
+++ Lab1/syn/synthPIPE.tcl
@@ -1,2 +1,13 @@
+#This script synthesizes the UNFOLDED & PIPED architecture
+
 source ./definition.tcl
 
+set newPer [synth $pipede $pipeddir $pipeddir 10.0]
+
+synth $pipede $pipeddir $pipeddirNC $newPer
+
+set fp [open "NoCKGnewPer$pipedtag.outGG" w]
+puts $fp $newPer
+close $fp
+
+exit
