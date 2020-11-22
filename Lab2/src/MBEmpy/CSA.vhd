487ba982615a4d3181abc8c9ec68e81f83da23e2
francescofalconieri <56508000+francescofalconieri@users.noreply.github.com>
2020-11-22 13:08:06 +0100

    First CSA ent

---
+++ Lab2/src/MBEmpy/CSA.vhd
@@ -0,0 +1,24 @@
+library ieee;
+  use ieee.std_logic_1164.all;
+  use ieee.numeric_std.all;
+  use ieee.std_logic_signed.all;
+
+  entity CSA is
+    generic (N : integer := 32)
+      port (
+          IN0 : in unsigned (N-1 downto 0);
+          IN1 : in unsigned (N-1 downto 0);
+          IN2 : in unsigned (N-1 downto 0); 
+          S : in unsigned (N-1 downto 0);
+          C : in unsigned (N-1 downto 0);
+      );
+  end entity CSA;
+
+  architecture structural of CSA
+
+      
+  begin
+      
+      
+      
+  end architecture structural;
---
+++ Lab2/src/MBEmpy/HA.vhd
@@ -0,0 +1,16 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+USE ieee.numeric_std.all;
+
+ENTITY HA IS
+PORT(
+		A, B : IN STD_LOGIC;
+		SUM, CARRY : OUT STD_LOGIC
+	 );
+END HA;
+
+ARCHITECTURE struct OF HA IS
+BEGIN
+SUM <= A XOR B;
+CARRY <= A AND B;
+END BEHAVIOUR;
