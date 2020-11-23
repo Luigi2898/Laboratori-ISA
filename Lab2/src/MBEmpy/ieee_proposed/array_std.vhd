c5ab942ec024c6a927cf79a6e9d82e69fa2e3506
DavErrico <s276311@studenti.polito.it>
2020-11-23 13:14:43 +0100

    Update array_std.vhd

--- Lab2/src/MBEmpy/ieee_proposed/array_std.vhd
+++ Lab2/src/MBEmpy/ieee_proposed/array_std.vhd
@@ -5,7 +5,7 @@ use std.textio.all;
 
 
 package array_std is
-constant N_pp_bits: integer := 8;
-constant N_pp_array : integer := 5;
+constant N_pp_bits: integer := 32;
+constant N_pp_array : integer := 17;
 type PP_array is array (N_pp_array-1 downto 0) of std_logic_vector(N_pp_bits downto 0);
 end package array_std;
