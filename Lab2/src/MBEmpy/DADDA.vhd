54a652ec447ca6bb6f0c73ebd44ff8235e61e0a5
Luigi <luigi.giuffrida.98@gmail.com>
2020-11-24 19:48:01 +0100

    up

--- Lab2/src/MBEmpy/DADDA.vhd
+++ Lab2/src/MBEmpy/DADDA.vhd
@@ -33,21 +33,29 @@ architecture structural of DADDA is
 		    S, Co : out std_logic
     );
   end component HA;
-
-  component signExtender is
-    generic(N : integer := 32);
-    port (
-      PP      : in  PP_array;
-      PP_sign : in  std_logic_vector (N / 2 downto 0);
-      PP_ext  : out PP_array_ext
-    ) ;
-  end component signExtender ; 
   
-  signal PP_in : PP_array_ext;
+  signal PP_ext : PP_array_ext;
 
 begin
 
-  SE : signExtender generic map (N)
-                    port map (PP, PP_sign, PP_in);
+  PP_ext(0)(N + 4 downto 1) <= not(PP_sign(0)) & PP_sign(0) & PP_sign(0) & PP(0)(N downto 0);
+
+  sign_extender : for i in 1 to N_pp_array - 3 generate
+    
+    PP_ext(i)(0)              <= PP_sign(i - 1);
+    PP_ext(i)(N + 2 downto 2) <= PP(i)(N downto 0);
+    PP_ext(i)(N + 3)          <= not(PP_sign(i));
+    PP_ext(i)(N + 4)          <= '1';
+    
+  end generate ; -- sign_extender
+  
+  PP_ext(N_PP_array - 2)(0)              <= PP_sign(N_PP_array - 3);
+  PP_ext(N_PP_array - 2)(N + 2 downto 2) <= PP(N_PP_array - 2)(N downto 0);
+  PP_ext(N_PP_array - 2)(N + 3)          <= not(PP_sign(N_PP_array - 2));
+
+
+  PP_ext(N_PP_array - 1)(0)                  <= PP_sign(N_PP_array - 2);
+  PP_ext(N_PP_array - 1)(N + 2 downto 2)     <= PP(N_PP_array - 1)(N downto 0);
+
 
 end architecture ;
