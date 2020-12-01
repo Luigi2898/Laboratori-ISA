87c0f7a5a3a7e7982a03cd735169a126027c9756
francescofalconieri <56508000+francescofalconieri@users.noreply.github.com>
2020-12-01 16:42:43 +0100

    Update DaddaScriptVerifier.m

--- Lab2/matlab/DaddaScriptVerifier.m
+++ Lab2/matlab/DaddaScriptVerifier.m
@@ -3,6 +3,12 @@ clc
 clear all
 close all
 
+fprintf('                ********************************************\n');
+fprintf('                ******** DADDA TREE SCRIPT VERIFIER ********\n');
+fprintf('                ********************************************\n\n');
+fprintf('Welcome to the verification app for the Dadda Tree HDL Generator.\n');
+fprintf('The search path for the input file (dot diagram output of the HDL generator) is:\n--> ../DaddaHDLGenerator/.\n\n');
+
 script_filename = input('Enter the script output file name (default is outDotNotation.txt): \','s');
 if script_filename == ""
     script_filename = 'outDotNotation.txt';
@@ -38,7 +44,7 @@ end
 fclose(fID_input);
 
 
-figure()
+figure(1)
 spy(levelsTensor(:,:,1),20,'y');
 hold on
 spy(levelsTensor(:,:,2),20,'c');
@@ -48,6 +54,10 @@ spy(levelsTensor(:,:,5),20,'b');
 spy(levelsTensor(:,:,6),20,'m');
 spy(levelsTensor(:,:,7),20,'k');
 grid minor
+
+fprintf('\nA dot notation plot of the HDL generator output has been produced in Figure (1).\n\n');
+
+
 
 levelElementsCorrect(1,:) = [0	2	3	3	4	4	5	5	6	6	7	7	8	8	9	9	10	10	11	11	12	12	13	13	14	14	15	15	16	17	17	17	17	16	17	15	16	14	15	13	14	12	13	11	12	10	11	9	10	8	9	7	8	6	7	5	6	4	5	3	4	2	3	1	2];
 levelElementsCorrect(2,:) = [0	2	3	3	4	4	5	5	6	6	7	7	8	8	9	9	10	10	11	11	12	13	13	13	13	13	13	13	13	13	13	13	13	13	13	13	13	13	13	13	13	12	13	11	12	10	11	9	10	8	9	7	8	6	7	5	6	4	5	3	4	2	3	1	2];
@@ -122,6 +132,8 @@ end
 
 fclose(fID_out);
 
+fprintf('\nA text file named "%s" containing the table of the excess bits\nat each level has been produced following the specified file name.\n',outfilename);
+
 j = 1;
 for i = 1:3:19
     toPlot(i,:) = levelElementsCorrect(j,:);
@@ -152,7 +164,9 @@ for i=1:65
 end
 
 
-figure()
+figure(2)
 tablePlot(toPlot,toPlotMask,headerRowPlot,headerColPlot);
 title({'DADDA HDL GENERATOR SCRIPT VERIFICATION REPORT TABLE',...
     'The number reported in the DIFF row are intended as excess bits from the script result wrt the correct one'});
+
+fprintf('The same table is reported in Figure (2).');
