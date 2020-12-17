1beb8e73afc14b9291bf440ddda04e4b1931f520
Luigi <luigi.giuffrida.98@gmail.com>
2020-12-17 18:05:27 +0100

    up

--- Lab2/matlab/tredplotGen.m
+++ Lab2/matlab/tredplotGen.m
@@ -2,23 +2,23 @@ close all
 clear all
 clc
 
-AreaBasic = 5621.643961;
-AreaCSA =   5072.087958;
-AreaPP = 4345.641962;
+AreaBasic = 3749.26;
+AreaCSA = 4803.69;
+AreaPP = 3547.37;
 
 Area = [AreaPP AreaBasic AreaCSA];
 Area = Area/max(Area);
 
-PeriodBasic = 1.60;
-PeriodCSA = 1.58;
-PeriodPP = 1.56;
+PeriodBasic = 5.03;
+PeriodCSA = 4.454;
+PeriodPP = 5.09;
 
 Period = [PeriodPP PeriodBasic PeriodCSA];
 Period = Period / max(Period);
 
-PowerBasic = 1.4690;
-PowerCSA = 1.2068;
-PowerPP = 1.2040;
+PowerBasic = 1.665;
+PowerCSA = 1.202;
+PowerPP = 1.040;
 
 Power = [PowerPP PowerBasic PowerCSA];
 Power = Power/max(Power);
@@ -28,7 +28,7 @@ toBPlt = [Area; Period; Power];
 f = figure
 bar(toBPlt)
 xticklabels(["Area" "Delay" "Power"])
-legend('Auto', 'PPARCH', 'CSA', 'Location', 'SouthEast')
+legend('PPARCH', 'Auto', 'CSA', 'Location', 'SouthEast')
 saveas(f, 'd3_impl.jpg')
 
 
@@ -52,9 +52,9 @@ DelayWALLACE = 0.73;
 DelayMBE = [DelayBEH DelayDADDA DelayWALLACE];
 DelayMBE = DelayMBE / max(DelayMBE);
 
-PowerBEH = 1.2250;
-PowerDADDA = 2.9864;
-PowerWALLACE = 2.2278;
+PowerBEH = 1.324;
+PowerDADDA = 2.053;
+PowerWALLACE = 2.087;
 
 PowerMBE = [PowerBEH PowerDADDA PowerWALLACE];
 PowerMBE = PowerMBE / max(PowerMBE);
@@ -64,8 +64,87 @@ toBPlt = [AreaMBE; DelayMBE; PowerMBE];
 f = figure
 bar(toBPlt)
 xticklabels(["Area" "Delay" "Power"])
-saveas(f, 'MBE.jpg')
 legend('Behavioural', 'Dadda', 'Wallace', 'Location', 'SouthEast')
+saveas(f, 'optall.jpg')
+
+
+
+
+
+
+
+
+
+
+AreaBEH = 4429.96;
+AreaDADDA = 6126.51;
+AreaWALLACE = 6234.5;
+
+AreaMBE = [AreaBEH AreaDADDA AreaWALLACE];
+AreaMBE = AreaMBE / max(AreaMBE);
+
+DelayBEH = 1.27;
+DelayDADDA = 1.46;
+DelayWALLACE = 1.70;
+
+DelayMBE = [DelayBEH DelayDADDA DelayWALLACE];
+DelayMBE = DelayMBE / max(DelayMBE);
+
+PowerBEH = 1.225;
+PowerDADDA = 2.986;
+PowerWALLACE = 2.595;
+
+PowerMBE = [PowerBEH PowerDADDA PowerWALLACE];
+PowerMBE = PowerMBE / max(PowerMBE);
+
+toBPlt = [AreaMBE; DelayMBE; PowerMBE];
+
+f = figure
+bar(toBPlt)
+xticklabels(["Area" "Delay" "Power"])
+legend('Behavioural', 'Dadda', 'Wallace', 'Location', 'SouthEast')
+saveas(f, 'compultraall.jpg')
+
+
+
+
+
+
+
+
+
+
+AreaBasic = 3749.26;
+AreaCSA = 4803.69;
+AreaPP = 3547.37;
+AreaDADDA = 
+AreaWA = 
+
+
+Area = [AreaPP AreaBasic AreaCSA];
+Area = Area/max(Area);
+
+PeriodBasic = 5.03;
+PeriodCSA = 4.454;
+PeriodPP = 5.09;
+
+Period = [PeriodPP PeriodBasic PeriodCSA];
+Period = Period / max(Period);
+
+PowerBasic = 1.665;
+PowerCSA = 1.202;
+PowerPP = 1.040;
+
+Power = [PowerPP PowerBasic PowerCSA];
+Power = Power/max(Power);
+
+toBPlt = [Area; Period; Power];
+
+f = figure
+bar(toBPlt)
+xticklabels(["Area" "Delay" "Power"])
+legend('PPARCH', 'Auto', 'CSA', 'Location', 'SouthEast')
+saveas(f, 'd3_impl.jpg')
 
 
 
