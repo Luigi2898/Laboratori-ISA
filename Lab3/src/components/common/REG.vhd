849c7aee86b216141a8964e8dbda7f46e127d4ad
luigi2898 <luigi.giuffrida.98@gmail.com>
2021-01-03 12:34:22 +0100

    Fixed styling og REG.vhd

---
+++ Lab3/src/components/common/REG.vhd
@@ -0,0 +1,28 @@
+library ieee;
+use ieee.std_logic_1146.all;
+
+entity REG is
+  generic(N : integer := 11);
+  port(
+	REG_IN           : in  std_logic_vector(N-1 downto 0);
+	CLK, RST_N, LOAD : in  std_logic;
+	REG_OUT          : out std_logic_vector(N-1 downto 0)
+  );
+end entity;
+
+architecture beh of REG is
+
+  begin
+	
+	register : process(CLK, RST_N)
+	begin
+	  if(RST_N = '0') then
+	    REG_OUT <= (others => '0');
+      elsif(CLK'event and CLK = '1') then
+	    if(LOAD = '1') then
+	      REG_OUT <= REG_IN;
+	    end if;
+      end if;
+	end process;
+
+end architecture;
