6edeaef3c8d50e51e7b8320a11c81f484f2b9d98
luigi2898 <luigi.giuffrida.98@gmail.com>
2021-01-12 17:19:54 +0100

    merging Dave to main

--- Lab3/src/RISC_V.vhd
+++ Lab3/src/RISC_V.vhd
@@ -163,8 +163,59 @@ architecture rtl of RISC_V is
       -- Datapath reset
       DP_RST         : out std_logic
     );
-  end component;  
-  
+  end component;
+
+  component PIPE_ID_EX is 
+  generic( word_size :  integer := 32 );
+  port(
+    CLK                   : in std_logic;
+    RST                   : in std_logic;
+    FLUSH                 : in std_logic;
+    STALL                 : in std_logic; 
+    RS1_VAL_IN            : in std_logic_vector(word_size-1 downto 0);
+    RS2_VAL_IN            : in std_logic_vector(word_size-1 downto 0);
+    IMM_GEN_IN            : in std_logic_vector(word_size-1 downto 0);
+    RS1_ADDR_IN           : in std_logic_vector(4 downto 0);
+    RS2_ADDR_IN           : in std_logic_vector(4 downto 0);
+    RD_ADDR_IN            : in std_logic_vector(4 downto 0);
+    WR_RFEN_IN            : in std_logic;
+    WR_RFMUX_IN           : in std_logic;
+    JUMP_IN               : in std_logic;
+    M_RD_EN_IN            : in std_logic;
+    M_WR_IN               : in std_logic;
+    EX_ALUSRC_IN          : in std_logic;
+    EX_ALUCTRL_IN         : in std_logic;
+    EX_ALUEN_IN           : in std_logic;
+    ------------------------------------------------------------------ out
+    WR_RFEN_OUT           : out std_logic;
+    WR_RFMUX_OUT          : out std_logic;
+    JUMP_OUT              : out std_logic;
+    M_RD_EN_OUT           : out std_logic;
+    M_WR_OUT              : out std_logic;
+    EX_ALUSRC_OUT         : out std_logic;
+    EX_ALUCTRL_OUT        : out std_logic;
+    EX_ALUEN_OUT          : out std_logic;
+    RS1_VAL_OUT           : out std_logic_vector(word_size-1 downto 0);
+    RS2_VAL_OUT           : out std_logic_vector(word_size-1 downto 0);
+    IMM_GEN_OUT           : out std_logic_vector(word_size-1 downto 0);
+    RS1_ADDR_OUT          : out std_logic_vector(4 downto 0);
+    RS2_ADDR_OUT          : out std_logic_vector(4 downto 0);
+    RD_ADDR_OUT           : out std_logic_vector(4 downto 0)    
+  );
+end component PIPE_ID_EX;
+
+component ALU is
+  generic (N : integer := 32)
+  port (
+    DATA1_IN   : in  std_logic_vector(N - 1 downto 0);
+    DATA2_IN   : in  std_logic_vector(N - 1 downto 0);
+    OPCODE_IN  : in  std_logic_vector(5 downto 0);
+    ZF_OUT     : out std_logic;
+    NF_OUT     : out std_logic;
+    RESULT_OUT : out std_logic_vector(N - 1 downto 0)
+  );
+end component;
+
   component ALU_CTRL is
     port (
       EN_IN    : in  std_logic;
@@ -173,6 +224,23 @@ architecture rtl of RISC_V is
       CODE_OUT : out std_logic_vector(5 downto 0)
     );
   end component;
+
+  component PIPE_EX_MEM is  
+  generic( word_size :  integer := 32 );
+  port(
+    CLK           : in  std_logic;
+    RST           : in  std_logic;
+    ALU_RES_IN    : in  std_logic_vector(word_size-1 downto 0);
+    RS2_VAL_IN    : in  std_logic_vector(word_size-1 downto 0); --from mux
+    OP_WB_MEM_IN  : in  std_logic_vector(4 downto 0); --MEM/WB ctrls (WB_RFEN_OUT & WB_RFMUX_OUT & BRANCH & M_RD_OUT & M_WR_OUT)
+    RD_ADDR_IN    : in  std_logic_vector(4 downto 0);
+    ------------------------------------------------------------------ out
+    ALU_RES_OUT   : out std_logic_vector(word_size-1 downto 0);
+    RS2_VAL_OUT   : out std_logic_vector(word_size-1 downto 0);
+    OP_WB_MEM_OUT : out std_logic_vector(4 downto 0);
+    RD_ADDR_OUT   : out std_logic_vector(4 downto 0)
+  );
+end component PIPE_EX_MEM;
 
   -- Global signals  
   signal Vdd, GND       : std_logic;
@@ -195,6 +263,20 @@ architecture rtl of RISC_V is
   signal BC_COMP1       : std_logic_vector(31 downto 0);
   signal BC_COMP2       : std_logic_vector(31 downto 0);
   signal ALU_CODE       : std_logic_vector(5 downto 0);
+  signal RS1_VAL_IDEX_OUT     : std_logic_vector(31 downto 0);
+  signal RS2_VAL_IDEX_OUT     : std_logic_vector(31 downto 0);
+  signal IMM_GEN_IDEX_OUT     : std_logic_vector(31 downto 0);
+  signal RS1_ADDR_IDEX_OUT    : std_logic_vector(4 downto 0);
+  signal RS2_ADDR_IDEX_OUT    : std_logic_vector(4 downto 0);
+  signal RD_ADDR_IDEX_OUT     : std_logic_vector(4 downto 0);
+  signal WR_RFEN_IDEX_OUT     : std_logic;
+  signal WR_RFMUX_IDEX_OUT    : std_logic;
+  signal JUMP_IDEX_OUT        : std_logic;
+  signal M_RD_EN_IDEX_OUT     : std_logic;
+  signal M_WR_IDEX_OUT        : std_logic;
+  signal EX_ALUSRC_IDEX_OUT   : std_logic;
+  signal EX_ALUCTRL_IDEX_OUT  : std_logic;
+  signal EX_ALUEN_IDEX_OUT    : std_logic;
   -- CU signals
   signal FLUSH          : std_logic;
   signal STALL          : std_logic;
@@ -216,7 +298,16 @@ architecture rtl of RISC_V is
   signal JUMP           : std_logic;
   signal FORWARD_B      : std_logic;
   signal FORWARD_A      : std_logic;
-
+-- Execute stage signals
+signal ALU_IN1_EXE          : std_logic_vector(31 downto 0);
+signal ALU_IN2_EXE          : std_logic_vector(31 downto 0);
+signal ALU_RES_EXE          : std_logic_vector(31 downto 0);
+signal CODE_ALUCTRL_OUT     : std_logic_vector(5 downto 0);
+signal OP_WB_MEM_PIPE_IN    : std_logic_vector(4 downto 0);  
+signal ALU_RES_PIPE_OUT     : std_logic_vector(31 downto 0);
+signal RS2_VAL_PIPE_OUT     : std_logic_vector(31 downto 0);
+signal OP_WB_MEM_PIPE_OUT   : std_logic_vector(4 downto 0);
+signal RD_ADDR_PIPE_OUT     : std_logic_vector(4 downto 0);
 begin
 
   Vdd <= '1';
@@ -263,5 +354,28 @@ begin
   CONTROL_UNIT : CU port map(EXTERNAL_RST, INSTR_ID(6 downto 0), BPU_MISSPRED, BPU_PREDICTION, HDU_STALL, HDU_FORWARD, ALU_SRC, ALU_CTRL, ALU_CTRL_EN, MEM_RD, MEM_WR, RF_EN, RF_MUX, IMM_EN, IMM_CODE, JUMP, FORWARD_B, FORWARD_A, I_RST);
 
   ALU_CONTROLLER : ALU_CTRL port map(ALU_CTRL_EN, ALU_CTRL, INSTR_ID(9 downto 7), ALU_CODE)
+
+  PIPE_REG2 : PIPE_ID_EXE generic map(32)
+                          port map(CLK, I_RST, PIPE_FLUSH, PIPE_STALL, BC_IN1, BC_IN2, IMM_GEN_OUT,
+                                   INSTR_ID(11 downto 7), INSTR_ID(20 downto 16), INSTR_ID(24 downto 20),
+                                   WR_RFEN, WR_RFMUX, JUMP, M_RD_EN, M_WR, EX_ALUSRC, EX_ALUCTRL, EX_ALUEN,
+                                   WR_RFEN_IDEX_OUT, WR_RFMUX_IDEX_OUT, JUMP_IDEX_OUT, M_RD_EN_IDEX_OUT, M_WR_IDEX_OUT, EX_ALUSRC_IDEX_OUT, EX_ALUCTRL_IDEX_OUT, EX_ALUEN_IDEX_OUT,
+                                   RS1_VAL_IDEX_OUT, RS2_VAL_IDEX_OUT, IMM_GEN_IDEX_OUT, RS1_ADDR_IDEX_OUT, RS2_ADDR_IDEX_OUT, RD_ADDR_IDEX_OUT);
+
+  ----------- Instruction execute stage -----------
+
+  MUX_ALU_IN2 : MUX2to1 generic map(32)
+                        port map(RS2_VAL_IDEX_OUT, IMM_GEN_IDEX_OUT, EX_ALUSRC_OUT, ALU_IN2_EXE);
+  
+  ALU_EXE : ALU generic map(32)
+                port map(RS1_VAL_IDEX_OUT, ALU_IN2_EXE, CODE_ALUCTRL_OUT, ALU_RES_EXE);
+
+  ALU_CTRL_EXE : ALE_CTRL port map(EX_ALUEN_IDEX_OUT, EX_ALUCTRL_IDEX_OUT, IMM_GEN_IDEX_OUT(14 downto 12), CODE_ALUCTRL_OUT);              
+
+  OP_WB_MEM_PIPE_IN <= WR_RFEN_IDEX_OUT & WR_RFMUX_IDEX_OUT & JUMP_IDEX_OUT & M_RD_EN_IDEX_OUT & M_WR_IDEX_OUT;
+
+  PIPE_REG3 : PIPE_EX_MEM generic map(32)
+                          port map(CLK, I_RST, ALU_RES_EXE, ALU_IN2_EXE, OP_WB_MEM_PIPE_IN, RD_ADDR_IDEX_OUT,
+                          ALU_RES_PIPE_OUT, RS2_VAL_PIPE_OUT, OP_WB_MEM_PIPE_OUT, RD_ADDR_PIPE_OUT);
 
 end architecture;
