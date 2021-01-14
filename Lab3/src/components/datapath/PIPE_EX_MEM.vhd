ce39f0e43e9500fbd37cc7da668f07d44164e3a6
DavErrico <s276311@studenti.polito.it>
2021-01-14 20:05:53 +0100

    fix up

---
+++ Lab3/src/components/datapath/PIPE_EX_MEM.vhd
@@ -0,0 +1,58 @@
+library ieee;
+  use ieee.std_logic_1164.all;
+  use ieee.numeric_std.all;
+
+
+entity PIPE_EX_MEM is 
+  port(
+    CLK           : in  std_logic;
+    RSTN          : in  std_logic;
+    ALU_RES_IN    : in  std_logic_vector(31 downto 0);
+    RS2_VAL_IN    : in  std_logic_vector(31 downto 0); --from mux
+    OP_WB_MEM_IN  : in  std_logic_vector(4 downto 0); --MEM/WB ctrls (WB_RFEN_OUT & WB_RFMUX_OUT & BRANCH & M_RD_OUT & M_WR_OUT)
+    RD_ADDR_IN    : in  std_logic_vector(4 downto 0);
+    ------------------------------------------------------------------ out
+    ALU_RES_OUT   : out std_logic_vector(31 downto 0);
+    RS2_VAL_OUT   : out std_logic_vector(31 downto 0);
+    OP_WB_MEM_OUT : out std_logic_vector(4 downto 0);
+    RD_ADDR_OUT   : out std_logic_vector(4 downto 0)
+  );
+end entity PIPE_EX_MEM;
+
+
+architecture beh of PIPE_EX_MEM is
+
+signal  ALU_RES   :  std_logic_vector(31 downto 0);
+signal  RS2_VAL   :  std_logic_vector(31 downto 0);
+signal  OP_WB_MEM :  std_logic_vector(4 downto 0);
+signal  RD_ADDR   :  std_logic_vector(4 downto 0);    
+
+begin
+
+pipe_id_exe_process : process(CLK, RSTN)
+begin
+    if(RSTN = '0') then
+
+        
+        ALU_RES   <= (others => '0');
+        RS2_VAL   <= (others => '0');
+        RD_ADDR   <= (others => '0');
+        OP_WB_MEM <= (others => '0');
+    
+    elsif (CLK'event and CLK = '1') then
+      
+        ALU_RES   <=   ALU_RES_IN;
+        RS2_VAL   <=   RS2_VAL_IN;
+        RD_ADDR   <=   RD_ADDR_IN;
+        OP_WB_MEM <=   OP_WB_MEM_IN; 
+
+    end if;
+
+end process; --pipe_id_exe_process   
+
+ALU_RES_OUT   <= ALU_RES;
+RS2_VAL_OUT   <= RS2_VAL;
+RD_ADDR_OUT   <= RD_ADDR;
+OP_WB_MEM_OUT <= OP_WB_MEM;
+
+end architecture beh;
---
+++ Lab3/src/components/datapath/PIPE_IF_ID.vhd
@@ -0,0 +1,61 @@
+library ieee;
+  use ieee.std_logic_1164.all;
+  use ieee.numeric_std.all;
+
+
+entity PIPE_IF_ID is 
+  port(
+    CLK               : in  std_logic;
+    RSTN              : in  std_logic;
+    FLUSH             : in  std_logic;
+    STALL             : in  std_logic; 
+    INSTR_IF_IN       : in  std_logic_vector(31 downto 0);
+    INSTR_PC_ADDR_IN  : in  std_logic_vector(31 downto 0);
+    INSTR_IF_OUT      : out std_logic_vector(31 downto 0);
+    INSTR_PC_ADDR_OUT : out std_logic_vector(31 downto 0)
+  );
+end entity PIPE_IF_ID;
+
+
+architecture beh of PIPE_IF_ID is
+
+signal IF_WORD_IN, PC_ADDR_IN : std_logic_vector(31 downto 0); 
+constant NOP                  : std_logic_vector(31 downto 0) := "00000000000000000000000000010011";
+
+  
+    
+begin
+
+pipe_if_id_process : process(CLK, RSTN)
+begin
+    if(RSTN = '0') then
+
+        IF_WORD_IN <= NOP;
+        PC_ADDR_IN <= (others => '0');
+    
+    elsif (CLK'event and CLK = '1') then
+      
+        if(FLUSH = '1') then --flush
+          
+          IF_WORD_IN <= NOP;
+          PC_ADDR_IN <= NOP_PC;
+        
+        elsif(FLUSH = '0' and STALL = '1') then --stall
+          
+          NULL;
+        
+        elsif(FLUSH = '0' and STALL = '0') then --normal exe  
+
+          IF_WORD_IN <= INSTR_IF_IN;
+          PC_ADDR_IN <= INSTR_PC_ADDR_IN;
+        
+        end if;
+
+    end if;
+
+end process; --pipe_if_id_process   
+
+INSTR_IF_OUT <= IF_WORD_IN;
+INSTR_PC_ADDR_OUT <= PC_ADDR_IN;
+
+end architecture beh;
