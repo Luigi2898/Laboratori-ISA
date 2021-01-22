939dd3b5f66d5adb3ca73413badf6011af327a99
DavErrico <s276311@studenti.polito.it>
2021-01-22 20:28:33 +0100

    Update DATA_MEM.vhd

--- Lab3/tb/DATA_MEM.vhd
+++ Lab3/tb/DATA_MEM.vhd
@@ -2,7 +2,6 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 use std.textio.all;
-use ieee.std_logic_textio.all;
 
 entity DATA_MEM is
 	generic( word_size     : integer := 32;
@@ -21,33 +20,39 @@ end entity DATA_MEM;
 architecture beh of DATA_MEM is
 	
 	type MEM_TYPE is array (0 to (2**word_size)-1) of std_logic_vector(word_size-1 downto 0);
+	
+	impure function initromfromfile return MEM_TYPE is
+        file DATA_FILE : text open read_mode is filename;
+        variable DATA_LINE : line;
+        variable TEMP_BV : bit_vector(word_size - 1 downto 0);
+        variable MEM_CONTENT : MEM_TYPE;
+    begin
+        for i in 0 to (2 ** word_size)-1 loop
+            readline(data_file, data_line);
+            read(data_line, temp_bv);
+            MEM_CONTENT(i) := to_stdlogicvector(temp_bv);
+        end loop;
+        return MEM_CONTENT;
+    end function;
 
-	signal MEM: MEM_TYPE;
+    signal MEM : MEM_TYPE := initromfromfile;
+
 begin
+
 
 	file_proc: process(CLK)
 	file FILE_TO_READ: text;
 	variable FILE_LINE : line;
-	variable ADDR_VAR : integer := 0;
 	variable init : boolean := true;
 	variable INSTR : std_logic_vector(word_size-1 downto 0);
-    begin
-	if (init = true) then
-		init := false;
-        file_open(FILE_TO_READ,filename,READ_MODE);
-        while (not endfile(FILE_TO_READ)) loop
-              readline(FILE_TO_READ,FILE_LINE);
-              hread(FILE_LINE,INSTR);
-			  MEM(ADDR_VAR) <= INSTR;
-			  ADDR_VAR := ADDR_VAR+1;
-              end loop;    
-		elsif (CLK'EVENT AND CLK = '1') then			
-			    if WR_EN='1' then
-				   MEM(to_integer(unsigned(ADDR))) <= DATA_IN;
-				end if;
-				if RD_EN='1' then
-				   DATA_OUT <= MEM(to_integer(unsigned(ADDR)));	
-				end if;   
+    begin  
+		if (CLK'EVENT AND CLK = '1') then			
+			if WR_EN='1' then
+			   MEM(to_integer(unsigned(ADDR))) <= DATA_IN;
+			end if;
+			if RD_EN='1' then
+			   DATA_OUT <= MEM(to_integer(unsigned(ADDR)));	
+			end if;   
 		end if;
 	end process file_proc;
 	
