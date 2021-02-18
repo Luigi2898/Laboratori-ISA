880923c8866d606d0a175456bc98ae904d528e27
DavErrico <s276311@studenti.polito.it>
2021-02-18 14:43:21 +0100

    up

---
+++ Lab3/innovus/RISC_V_v2/sim/post_pr_v2.sh
@@ -0,0 +1,21 @@
+#mkdir vcd
+
+source /software/scripts/init_msim6.5c
+vlib work
+
+vcom -93 -work ./work ../../../src/components/common/*.vhd
+vcom -93 -work ./work ../../../src/components/control/*.vhd
+vcom -93 -work ./work ../../../src/components/control/BPU/*.vhd
+vcom -93 -work ./work ../../../src/components/control/BPU/BPU_32bit/*.vhd
+vcom -93 -work ./work ../../../src/components/datapath/*.vhd
+vcom -93 -work ./work ../../../src/components/testbenchComponents/*.vhd
+vcom -93 -work ./work ../../../src/*.vhd
+vlog -work ./work ../../../tb/MAIN_MEM.v
+vlog -work ./work ../../../tb/TB_RISCV_v2_back.v
+vlog -work ./work ../RISC_V_v2.v
+
+#vsim -c -do "vsim -voptargs=+acc -c -L /software/dk/nangate45/verilog/msim6.5c -sdftyp /TB_RISCV_v2_back/DUT=../RISC_V_v2.sdf work.TB_RISCV_v2_back -t 1ps; add wave *; vcd file ./vcd/RISC_V_v2.vcd; vcd add /TB_RISCV_v2_back/DUT/*; run 60 us"
+#vsim -c -do "vsim -voptargs=+acc -c -L /software/dk/nangate45/verilog/msim6.5c -sdftyp /TB_RISCV_v2_back/DUT=../RISC_V_v2.sdf work.TB_RISCV_v2_back -t 1ps; add wave *; vcd file ./vcd/RISC_V_v2_matrix.vcd; vcd add /TB_RISCV_v2_back/DUT/*; run 300 us"
+#vsim -c -do "vsim -voptargs=+acc -c -L /software/dk/nangate45/verilog/msim6.5c -sdftyp /TB_RISCV_v2_back/DUT=../RISC_V_v2.sdf work.TB_RISCV_v2_back -t 1ps; add wave *; vcd file ./vcd/RISC_V_v2_min_src.vcd; vcd add /TB_RISCV_v2_back/DUT/*; run 300 us"
+vsim -c -do "vsim -voptargs=+acc -c -L /software/dk/nangate45/verilog/msim6.5c -sdftyp /TB_RISCV_v2_back/DUT=../RISC_V_v2.sdf work.TB_RISCV_v2_back -t 1ps; add wave *; vcd file ./vcd/RISC_V_v2_neg_cnt.vcd; vcd add /TB_RISCV_v2_back/DUT/*; run 300 us"
+
