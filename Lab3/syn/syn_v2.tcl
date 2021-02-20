c67396b39d7e66668fa7156ef03bda99ee5b47e6
DavErrico <s276311@studenti.polito.it>
2021-02-20 13:50:04 +0100

    up

--- Lab3/syn/syn_v2.tcl
+++ Lab3/syn/syn_v2.tcl
@@ -1,8 +1,8 @@
 #*****************************************************************************
 # This script is used to synthesize the RISC-V 
 #*****************************************************************************
-sh rm -r ./work
-sh mkdir work
+#sh rm -r ./WORK
+sh mkdir WORK
 sh mkdir logs/RISC_V_v2
 sh mkdir reports/RISC_V_v2
 sh mkdir reports/RISC_V_v2/netlist
@@ -17,7 +17,7 @@ analyze -library WORK -format vhdl {../src/components/common/MUX_2X1TO1X1.vhd}
 analyze -library WORK -format vhdl {../src/components/common/MUX_4to1.vhd}
 analyze -library WORK -format vhdl {../src/components/common/REG.vhd}
 analyze -library WORK -format vhdl {../src/components/common/SIPO_SHIFT_REG.vhd}
-analyze -library WORK -format vhdl {../src/components/control/ALU_CTRL.vhd}
+analyze -library WORK -format vhdl {../src/components/control/ALU_CTRL_v2.vhd}
 analyze -library WORK -format vhdl {../src/components/control/CU.vhd}
 analyze -library WORK -format vhdl {../src/components/control/HDU.vhd}
 analyze -library WORK -format vhdl {../src/components/control/BPU/AGE_CNT.vhd}
@@ -25,12 +25,12 @@ analyze -library WORK -format vhdl {../src/components/control/BPU/BHT.vhd}
 analyze -library WORK -format vhdl {../src/components/control/BPU/BPU_CU.vhd}
 analyze -library WORK -format vhdl {../src/components/control/BPU/CACHE_MEM.vhd}
 analyze -library WORK -format vhdl {../src/components/control/BPU/DELAY_CHAIN.vhd}
-analyze -library WORK -format vhdl {../src/components/control/BPU/DELAY_CHAIN1.vhd}
+analyze -library WORK -format vhdl {../src/components/control/BPU/DELAY_CHAIN_1.vhd}
 analyze -library WORK -format vhdl {../src/components/control/BPU/PHT.vhd}
 analyze -library WORK -format vhdl {../src/components/control/BPU/SAT_CNT.vhd}
 analyze -library WORK -format vhdl {../src/components/control/BPU/BPU_32bit/BPU.vhd}
 analyze -library WORK -format vhdl {../src/components/datapath/ABS_VAL.vhd}
-analyze -library WORK -format vhdl {../src/components/datapath/ALU.vhd}
+analyze -library WORK -format vhdl {../src/components/datapath/ALU_v2.vhd}
 analyze -library WORK -format vhdl {../src/components/datapath/BRANCH_COMP.vhd}
 analyze -library WORK -format vhdl {../src/components/datapath/IMM_GEN.vhd}
 analyze -library WORK -format vhdl {../src/components/datapath/INCREMENTER.vhd}
@@ -44,30 +44,31 @@ analyze -library WORK -format vhdl {../src/RISC_V_v2.vhd}
 
 
 #elaborate top entity, set the variables
-elaborate RISC_V -architecture rtl -library WORK > ./logs/RISC_V_v2/elaborate_v2.txt
+elaborate RISC_V_v2 -architecture rtl -library WORK > ./logs/RISC_V_v2/elaborate_v2.txt
 
 #**************** CONSTRAINT THE SYNTHESIS ****************#
 # setting design constrains
-create_clock -name MY_CLK -period 10 clk
+link
+create_clock -name MY_CLK -period 3 CLK
 set_dont_touch_network MY_CLK
 set_clock_uncertainty 0.07 [get_clocks MY_CLK]
-set_input_delay 0.5 -max -clock MY_CLK [remove_from_collection [all_inputs] clk]
+set_input_delay 0.5 -max -clock MY_CLK [remove_from_collection [all_inputs] CLK]
 set_output_delay 0.5 -max -clock MY_CLK [all_outputs]
 set OLOAD [load_of NangateOpenCellLibrary/BUF_X4/A]
 set_load $OLOAD [all_outputs]
 # compiling the RISC_V_v2
 compile -exact_map > ./logs/RISC_V_v2/compile_v2.txt
-report_timing -nworst 10 > ./reports/RISC_V_v2/timing_v2.txt
+report_timing > ./reports/RISC_V_v2/timing_v2.txt
 report_area > ./reports/RISC_V_v2/area_v2.txt
 report_power > ./reports/RISC_V_v2/power_v2.txt
 
-ungroup -all -flatten
-change_names -hierarchy -rules verilog
-write_sdf ./reports/RISC_V_v2/netlist/RISC_V_v2.sdf
-write -f verilog -hierarchy -output ./reports/RISC_V_v1/netlist/RISC_V_v2.v
-write_sdc ./reports/RISC_V_v2/netlist/RISC_V_v2.sdc
-write -hierarchy -format ddc -output ./reports/RISC_V_v2/netlist/RISC_V_v2.ddc
-quit
+#ungroup -all -flatten
+#change_names -hierarchy -rules verilog
+#write_sdf ./reports/RISC_V_v2/netlist/RISC_V_v2.sdf
+#write -f verilog -hierarchy -output ./reports/RISC_V_v2/netlist/RISC_V_v2.v
+#write_sdc ./reports/RISC_V_v2/netlist/RISC_V_v2.sdc
+#write -hierarchy -format ddc -output ./reports/RISC_V_v2/netlist/RISC_V_v2.ddc
+#quit
 
 
 
